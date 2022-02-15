0c662a483a52b563b7a44c274c793a023ede7b1b
player31 <keumks0331@gmail.com>
2022-02-15 20:02:20 +0900

    [Add] js-joda timezone

--- util.js
+++ util.js
@@ -1,13 +1,22 @@
 // https://jojoldu.tistory.com/600 이글을 참고해 보시면 좋을 것 같습니다!!!
-
-const { LocalDateTime, nativeJs } = require("@js-joda/core");
+const jsJoda = require("@js-joda/core");
+var { ZoneId, ZonedDateTime, nativeJs, ZoneOffset } = jsJoda;
+require("@js-joda/timezone");
 
 function transformLocalDate(data) {
-  data._doc.createdAt = LocalDateTime.from(nativeJs(data.createdAt)).toString();
-  data._doc.updatedAt = LocalDateTime.from(nativeJs(data.updatedAt)).toString();
+  const KOREA = ZoneId.of("Asia/Seoul");
+  data._doc.createdAt = ZonedDateTime.from(nativeJs(data.createdAt), KOREA).toString();
+  data._doc.updatedAt = ZonedDateTime.from(nativeJs(data.updatedAt), KOREA).toString();
   return data;
 }
 
 module.exports = {
   transformLocalDate,
 };
+
+//client에서 사용할때
+// const utcZoned = ZonedDateTime.of(ZonedDateTime.parse(reuslt.createdAt).toLocalDateTime(), ZoneOffset.UTC);
+// const KOREA = ZoneId.of("Asia/Seoul");
+// const korea = utcZoned.withZoneSameInstant(KOREA);
+// const koreaLocal = korea.toLocalDateTime();
+// console.log(koreaLocal.toString());
